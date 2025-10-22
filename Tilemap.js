b2de9c0f425cd92fca88c9db087a5153162d2422
stickman69420 <124035958+stickman69420@users.noreply.github.com>
2025-10-23 09:17:53 +1000

    Add files via upload

--- Tilemap.js
+++ Tilemap.js
@@ -1,9 +1,9 @@
-import loadImage from Images.js
+import("./Images.js")
 export function Tilemap(Context,Tileset,Tilemap) {
 	Let Map
 	Let Set = await loadImage(Tileset)
 	fetch(Tilemap)
     .then((response) => response.json())
     .then((json) => Map=json);
-	return ([Map,Set])
+	return (Map)
 }
